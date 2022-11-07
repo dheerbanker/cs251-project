63b4ee6848784abc161ae284e90edc04afe52fa4
DheGit <bankerdheer@gmail.com>
2022-11-07 18:48:57 +0530

    Add gitignore and client.py

--- client.py
+++ client.py
@@ -1,19 +1,39 @@
-# Import socket module
-import socket			
+# Python program to implement client side of chat room.
+import socket
+import select
+import sys
 
-# Create a socket object
-s = socket.socket()		
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if len(sys.argv) != 3:
+	print ("Correct usage: script, IP address, port number")
+	exit()
+IP_address = str(sys.argv[1])
+Port = int(sys.argv[2])
+server.connect((IP_address, Port))
 
-# Define the port on which you want to connect
+while True:
 
-# connect to the server on local computer
-server_ip = "0.0.0.0"
-my_ip = "49.32.147.164"
-server_port = 5458
-s.connect((server_ip, server_port))
+	# maintains a list of possible input streams
+	sockets_list = [sys.stdin, server]
 
-# receive data from the server and decoding to get the string.
-print (s.recv(1024).decode())
-# close the connection
-s.close()	
-	
+	""" There are two possible input situations. Either the
+	user wants to give manual input to send to other people,
+	or the server is sending a message to be printed on the
+	screen. Select returns from sockets_list, the stream that
+	is reader for input. So for example, if the server wants
+	to send a message, then the if condition will hold true
+	below.If the user wants to send a message, the else
+	condition will evaluate as true"""
+	read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
+
+	for socks in read_sockets:
+		if socks == server:
+			message = socks.recv(2048)
+			print (message)
+		else:
+			message = sys.stdin.readline()
+			server.send(message.encode('utf-8'))
+			sys.stdout.write("<You>")
+			sys.stdout.write(message)
+			sys.stdout.flush()
+server.close()
